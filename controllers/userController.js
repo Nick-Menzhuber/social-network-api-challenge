4f0b159b82ae99cb2f7bcd2f66317edf6c9aabe1
Nick Menzhuber <nickmenzhuber@Nicks-Mac-mini.local>
2023-01-16 17:04:55 -0600

    user routes tested and mostly good; delete routes wonky

--- controllers/userController.js
+++ controllers/userController.js
@@ -47,4 +47,47 @@ module.exports = {
             })
       );
   },
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true },
+    )
+        .then((user) =>
+            !user
+                ? res.status(404).json({ message: 'No user with this id' })
+                : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err));
+},
+  addFriend(req, res) {
+    console.log("You are adding a friend");
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body } },
+      { runValidators: true, new: true }
+    ).then((user) =>
+      !user
+        ? res
+            .status(404)
+            .json({ message: "No user found with that ID, sorry!" })
+        : res.json(user).catch((err) => res.status(500).json(err))
+    );
+  },
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { friend: { friendId: req.params.friendId } } },
+      { runValidators: true, new: true }
+    )
+        .then((user) =>
+          !user
+            ? res
+                .status(404)
+                .json({ message: "No user found with that ID, sorry!" })
+            : res.json(user)
+        )
+        .catch((err) => res.status(500).json(err))
+  },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -4,13 +4,21 @@ const {
   getOneUser,
   newUser,
   deleteUser,
-  //updateUser,
-  //addFriend,
-  //removeFriend,
+  updateUser,
+  addFriend,
+  removeFriend,
 } = require("../../controllers/userController");
 
+// routes for /api/users
 router.route("/").get(getUsers).post(newUser);
 
-router.route("/:userId").get(getOneUser).delete(deleteUser);
+// routes for /api/users/:userId
+router.route("/:userId").get(getOneUser).delete(deleteUser).put(updateUser);
+
+// route for /api/users/:userId/friends
+router.route("/:userId/friends").post(addFriend);
+
+//route for /api/users/:userId/friends/:friendId
+router.route("/:userId/friends/:friendId").delete(removeFriend)
 
 module.exports = router;
